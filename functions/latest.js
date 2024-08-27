ae12774e350e36712bfc7b45a5527f56cf7a805e
Crocopig <liadsman@gmail.com>
2024-08-28 02:32:09 +0300

    Update latest.js

--- functions/latest.js
+++ functions/latest.js
@@ -1,12 +1,12 @@
-exports.handler = async (event, context) => {
-  // Example static data for testing
-  const latestData = {
-    message: "Test message for debugging.",
-    title: "Test Title",
-    url: "https://www.example.com",
-    fileType: "PDF"
-  };
+// This will be used to store the most recent data
+let latestData = {
+  message: "",
+  title: "",
+  url: "",
+  fileType: ""
+};
 
+exports.handler = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify(latestData),
