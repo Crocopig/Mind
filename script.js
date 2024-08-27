fa1d64db031157ed9b3267c646eff5c7c31e527f
Crocopig <liadsman@gmail.com>
2024-08-28 01:17:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -35,17 +35,27 @@ function updateUI(data) {
     }
 }
 
-function getFileIconHTML(fileType) {
-    const iconColor = 'white';
-    switch(fileType.toLowerCase()) {
-        case 'pdf':
-            return `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="${iconColor}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>`;
-        case 'doc':
-        case 'docx':
-            return `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="${iconColor}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>`;
-        default:
-            return `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="${iconColor}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>`;
-    }
+function fadeOut(element) {
+    element.style.opacity = 1;
+    (function fade() {
+        if ((element.style.opacity -= .1) < 0) {
+            element.style.display = "none";
+        } else {
+            requestAnimationFrame(fade);
+        }
+    })();
+}
+
+function fadeIn(element, display) {
+    element.style.opacity = 0;
+    element.style.display = display || "block";
+    (function fade() {
+        var val = parseFloat(element.style.opacity);
+        if (!((val += .1) > 1)) {
+            element.style.opacity = val;
+            requestAnimationFrame(fade);
+        }
+    })();
 }
 
 function updateUIWithAnimation(data) {
